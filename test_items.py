81ca8e96b99d6008099c8daf2431e150a96286cd
cbet <npocbet@gmail.com>
2021-09-27 08:41:34 +0300

    changed test_items.py

--- test_items.py
+++ test_items.py
@@ -1,14 +1,11 @@
 import time
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_guest_should_see_login_link(browser):
+def test_there_is_a_button_add_item_to_basket(browser):
     browser.get(link)
-    event = EC.element_to_be_clickable((By.CLASS_NAME, "btn-add-to-basket"))
-    WebDriverWait(browser, 7).until(event)
     time.sleep(5)
+    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
