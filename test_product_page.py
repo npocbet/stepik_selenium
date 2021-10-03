5c30770493b78ebb029dfab293d31c44204a926a
cbet <npocbet@gmail.com>
2021-10-03 09:44:15 +0300

    test_product_page.py added some links to test, one of them xfailed

--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,29 @@
 import time
 
+import pytest
+
 from pages.product_page import ProductPage
 
+# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
-def test_correct_adding_item_to_the_buscet(browser):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+                                               "/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_correct_adding_item_to_the_buscet(browser,link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_correct_value_in_the_allert()
-    # time.sleep(1)
     page.solve_quiz_and_get_code()
     page.check_product_name_in_the_buscet()
     page.check_product_price_in_the_buscet()
-    time.sleep(5)
+    time.sleep(1)
